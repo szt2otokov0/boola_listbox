621b3ad6efe67c8866553c3ffeb297df4e8f6b2f
szt2otokov <szt2otokov@vasvari.org>
2023-09-15 12:30:48 +0200

    dun but it doesn't work

---
+++ WpfApp1/Model/Expense.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    record Expense(SpendProfile Profile, string Title, string AmountText, Currency Currency, string Description,
+        DateTime Date, bool Repeats, byte? RepeatRate, RepeatUnit? RepeatUnit
+        )
+    {
+        public Expense() : this(SpendProfile.Personal, "New expense", "0", Currency.HUF, "something", DateTime.UnixEpoch, false, null, null)
+        {
+        }
+
+        public double Amount => Convert.ToDouble(AmountText);
+    }
+
+    enum SpendProfile
+    {
+        Personal,
+        Work
+    }
+
+    enum Currency
+    {
+        HUF,
+        EUR,
+        USD,
+        XRD
+    }
+
+    enum RepeatUnit
+    {
+        Days,
+        Weeks,
+        Months,
+        Years
+    }
+}
---
+++ WpfApp1/ViewModel/ExpenseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    partial class ExpenseViewModel : ObservableObject
+    {
+
+        [ObservableProperty] private List<SpendProfile> spendProfiles = new() { SpendProfile.Personal,SpendProfile.Work};
+        [ObservableProperty] private List<Currency> currencies = new() { Currency.HUF,Currency.EUR,Currency.USD,Currency.XRD};
+        [ObservableProperty] private List<RepeatUnit> repeatUnits = new() { RepeatUnit.Days,RepeatUnit.Weeks,RepeatUnit.Months,RepeatUnit.Years };
+
+        [ObservableProperty] private Expense expense = new();
+    }
+}
