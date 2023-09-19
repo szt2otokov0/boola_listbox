6105e2decd53ce6ba3c37c649b7fea906c534753
szt2otokov <szt2otokov@vasvari.org>
2023-09-19 11:38:06 +0200

    almost

--- WpfApp1/ViewModel/ExpenseViewModel.cs
+++ WpfApp1/ViewModel/ExpenseViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using WpfApp1.Model;
 
 namespace WpfApp1.ViewModel
@@ -11,10 +13,57 @@ namespace WpfApp1.ViewModel
     internal partial class ExpenseViewModel : ObservableObject
     {
 
-        [ObservableProperty] private List<SpendProfile> spendProfiles = new() { SpendProfile.Personal,SpendProfile.Work};
-        [ObservableProperty] private List<Currency> currencies = new() { Currency.HUF,Currency.EUR,Currency.USD,Currency.XRD};
-        [ObservableProperty] private List<RepeatUnit> repeatUnits = new() { RepeatUnit.Days,RepeatUnit.Weeks,RepeatUnit.Months,RepeatUnit.Years };
+        [ObservableProperty] private ObservableCollection<SpendProfile> spendProfiles = new() { SpendProfile.Personal, SpendProfile.Work };
+        [ObservableProperty] private ObservableCollection<Currency> currencies = new() { Currency.HUF, Currency.EUR, Currency.USD, Currency.XRD };
+        [ObservableProperty] private ObservableCollection<RepeatUnit> repeatUnits = new() { RepeatUnit.Days, RepeatUnit.Weeks, RepeatUnit.Months, RepeatUnit.Years };
+        [ObservableProperty] private ObservableCollection<Expense> expenses = new();
 
-        [ObservableProperty] private Expense expense = new();
+        private SpendProfile selectedSpendProfile;
+        public SpendProfile SelectedSpendProfile
+        {
+            get => selectedSpendProfile;
+            set {
+                SetProperty(ref selectedSpendProfile, value);
+                SelectedExpense = SelectedExpense with { Profile = selectedSpendProfile};
+            }
+        }
+
+        private Currency selectedCurrency;
+        public Currency SelectedCurrency
+        {
+            get => selectedCurrency;
+            set {
+                SetProperty(ref selectedCurrency, value);
+                SelectedExpense = SelectedExpense with { Currency = selectedCurrency};
+            }
+        }
+
+        [ObservableProperty] private Expense selectedExpense = new();
+
+        public ExpenseViewModel()
+        {
+            selectedExpense = new();
+            expenses.Add(new Expense());
+        }
+
+        [RelayCommand]
+        public void Save(Expense expenseToSave)
+        {
+            Expenses.Add(expenseToSave);
+            OnPropertyChanged(nameof(Expenses));
+        }
+
+        [RelayCommand]
+        public void Create()
+        {
+            SelectedExpense = new();
+        }
+
+        [RelayCommand]
+        public void Delete(Expense expenseToDelete)
+        {
+            Expenses.Remove(expenseToDelete);
+            OnPropertyChanged(nameof(Expenses));
+        }
     }
 }
